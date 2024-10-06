fecf71659b81f8681a3a1674bf27be3434968ca0
Madis V. <146342746+madis789tl@users.noreply.github.com>
2024-10-06 17:16:59 +0300

    todos validation

--- 2024-09-23/typhoon/backend/routes/todo.routes.js
+++ 2024-09-23/typhoon/backend/routes/todo.routes.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { query } = require("express-validator");
 const router = express.Router();
 const todoController = require("../controllers/todo.controller");
 const {
@@ -9,8 +10,8 @@ const {
 router.use(todoRouteMiddleware);
 
 router.get("/", todoGetRouteMiddleware, todoController.read);
-router.post("/", todoController.create);
-router.put("/:id", todoController.update);
+router.post("/", query("req.body").notEmpty(), todoController.create);
+router.put("/:id", query("req.body").notEmpty(), todoController.update);
 router.delete("/:id", todoController.delete);
 
 module.exports = router;
