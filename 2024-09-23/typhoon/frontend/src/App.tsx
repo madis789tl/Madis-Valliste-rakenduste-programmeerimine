4858ff692cf524eda08c94ad5fd4ce324c2af10c
Madis V. <146342746+madis789tl@users.noreply.github.com>
2024-10-06 16:47:53 +0300

    Uus theme todoks
    Lisasin uue themei juurde, sinine ja punane.

--- 2024-09-23/typhoon/frontend/src/App.tsx
+++ 2024-09-23/typhoon/frontend/src/App.tsx
@@ -1,12 +1,37 @@
 import "./App.css";
 import Cats from "./components/Cats";
 import Todo from "./components/ToDo";
+import { ThemeProvider } from "@emotion/react";
+import { createTheme } from "@mui/material";
+import { red, blue } from "@mui/material/colors";
+
+
+const Theme = createTheme({
+  palette: {
+    primary: {
+      main: blue[500],
+    },
+  },
+});
+
+const doTheme = createTheme({
+  palette: {
+    primary: {
+      main: red[500],
+    },
+  },
+});
 
 function App() {
   return (
     <>
-      <Cats />
-      <Todo />
+      <ThemeProvider theme={Theme}>
+        <Cats />
+      </ThemeProvider>
+      <hr />
+      <ThemeProvider theme={doTheme}>
+        <Todo />
+      </ThemeProvider>
     </>
   );
 }
